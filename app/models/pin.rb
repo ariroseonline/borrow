086ddf2b80cae6c5a722210200b9c83b88f2798a
ariroseonline <ariroseonline@gmail.com>
2013-12-02 01:30:44 -0500

    implement paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,11 @@
 class Pin < ActiveRecord::Base
-	validates :description, presence:true, length: {maximum: 50}
-	validate :user, presence: true
+
+
 	belongs_to :user
+
+	validates :description, presence:true
+	validates :user_id, presence: true
+	validates :image, :attachment_presence => true
+	has_attached_file :image, styles: { medium: "300x240"}
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131130222917) do
+ActiveRecord::Schema.define(version: 20131201180527) do
 
   create_table "his", force: true do |t|
     t.datetime "created_at"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 20131130222917) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id"

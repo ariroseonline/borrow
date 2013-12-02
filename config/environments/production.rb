ca70f68184c12e5c0c5de7f68ff3760f9491852d
ariroseonline <ariroseonline@gmail.com>
2013-12-02 02:58:14 -0500

    heroku amazon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,13 +89,14 @@ Borrow::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   #AWS for paper clip
+
+
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => 'arisafari',
-      :url => ":s3_domain_url",
-      :access_key_id => 'AKIAIORRCXKHQWTSSLAQ',
-      :secret_access_key => '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'
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 
